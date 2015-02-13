24f0c8c152feb89d2f1fab1c04c32c0ab8ca65cd
Lucas Santos & Rosa Pergams <pair+lucasrsantos1.rosapergams@devbootcamp.com>
2015-02-13 11:32:21 -0500

    Add migrations

--- db/migrate/20150213112628_create_users.rb
+++ db/migrate/20150213112628_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :password_digest
+      t.string :email
+
+      t.timestamps
     end
   end
 end
