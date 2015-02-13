bc1e1c27e20c68a0f57eb66d53839d92bb89f7b3
Lucas Santos & Rosa Pergams <pair+lucasrsantos1.rosapergams@devbootcamp.com>
2015-02-13 14:43:32 -0500

    Add auth controller

---
+++ app/controllers/auth.rb
@@ -0,0 +1,34 @@
+get '/login' do
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/signup' do
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    redirect "/signup"
+  end
+end
+
+get '/signout' do
+  session[:user_id] = nil
+  redirect '/'
+end
